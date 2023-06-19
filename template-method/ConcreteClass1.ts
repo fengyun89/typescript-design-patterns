a950c5040ef63a20cc72673005979f5513649ba7
coding-freedom <iamcodingfreedom@gmail.com>
2023-06-19 17:58:02 +0800

    模板方法模式

---
+++ template-method/ConcreteClass1.ts
@@ -0,0 +1,13 @@
+import AbstractClass from "./AbstractClass";
+
+class ConcreteClass1 extends AbstractClass {
+  protected requiredOperations1(): void {
+    console.log("ConcreteClass1 says: Implemented Operation1");
+  }
+
+  protected requiredOperation2(): void {
+    console.log("ConcreteClass1 says: Implemented Operation2");
+  }
+}
+
+export default ConcreteClass1;
---
+++ template-method/index.ts
@@ -0,0 +1,8 @@
+import ConcreteClass1 from "./ConcreteClass1";
+import ConcreteClass2 from "./ConcreteClass2";
+
+const class1 = new ConcreteClass1();
+class1.templateMethod();
+
+const class2 = new ConcreteClass2();
+class2.templateMethod();
